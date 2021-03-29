d8273ae4547e49c3c9972eda1cf747b042813c71
Danial Tariq <danial09.dt@gmail.com>
2021-03-29 13:57:44 +0100

    Add required import

--- sudokugame/views.py
+++ sudokugame/views.py
@@ -1,7 +1,8 @@
 import sys
 from random import randrange
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse

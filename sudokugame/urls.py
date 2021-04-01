338f508bb2fff03a1d7adebe93b7e20f13621984
Danial Tariq <danial09.dt@gmail.com>
2021-04-01 20:19:22 +0100

    Add new play page.

--- sudokugame/urls.py
+++ sudokugame/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('home/', views.home, name='home'),
+    path('play/', views.play, name='play'),
     path('logout/', views.user_logout, name='logout')
 ]

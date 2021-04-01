82fd3a486cd095262656bad1f5483815f36f4796
Danial Tariq <danial09.dt@gmail.com>
2021-04-01 15:37:57 +0100

    Update the look of base.html.
    I would usually break this into multiple commits but I'm in a rush.

--- sudokugame/urls.py
+++ sudokugame/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('home/', views.home, name='home'),
+    path('logout/', views.user_logout, name='logout')
 ]

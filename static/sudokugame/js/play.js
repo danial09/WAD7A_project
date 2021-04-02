50e3d570d3281f9870365fa3d4b1b1cad9138af6
Danial Tariq <danial09.dt@gmail.com>
2021-04-02 13:00:52 +0100

    Add ability to move cell focus using the arrow keys

--- static/sudokugame/js/play.js
+++ static/sudokugame/js/play.js
@@ -2,7 +2,19 @@ $(document).ready(function() {
     $(".game-cell").on("click", (target) => {
         focusCell($(target.target).closest(".game-cell"));
     });
+    $("body").keydown((event) => {
+        console.log(event.key);
+        if (event.key.startsWith("Arrow")) {
+            event.preventDefault();
+            cellChangeFocus(event.key);
+        }
+    })
 })
+
+function mod(n, m) {
+    // Function to only get the positive mod result.
+    return ((n % m) + m) % m;
+}
 
 function focusCell(cell) {
     $("#focused-cell").removeAttr("id");
@@ -15,6 +27,26 @@ function focusCell(cell) {
     $(".game-cell:nth-child(" + ($(cell).index() + 1) + ")").addClass("related-col-cell");
     $(cell).removeClass("related-col-cell"); // Probably not necessary.
 
+}
+
+function cellChangeFocus(direction) {
+    const curCell = $("#focused-cell");
+    const curPosition = $(curCell).index();
+    let newCell = null;
+    if ($(curCell).length === 0) {
+        console.log("Hello");
+        newCell = $(".game-cell").first();
+    } else if (direction === "ArrowRight" || direction === "ArrowLeft") {
+        const newPosition = mod(curPosition + (direction === "ArrowRight" ? 1 : -1), 9);
+        console.log("new position: " + newPosition);
+        newCell = curCell.parent().children()[newPosition];
+    } else {
+        const curRow = $(curCell).parent();
+        const newRowPosition = mod($(curRow).index() + (direction === "ArrowDown" ? 1 : -1), 9);
+        const newRow = $(curRow).parent().children()[newRowPosition];
+        newCell = $(newRow).children()[curPosition];
+    }
+    focusCell(newCell);
 }
 
 function cellKeyDown(cell, event) {

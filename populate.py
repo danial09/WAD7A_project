43c62ab51a3067ac10104b0338cbcec24b5055e7
Danial Tariq <danial09.dt@gmail.com>
2021-04-05 23:18:32 +0100

    Update populate script.

--- populate.py
+++ populate.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WAD7A_project.settings')
 
@@ -7,162 +8,84 @@ import django
 django.setup()
 
 from sudokugame.models import Board, Game
-from sudokugame.sudoku_core import generate, flatten_join, solve
+from sudokugame.sudoku_core import generate, flatten_join, solve, get_flattened_info, generate_score
 from django.utils import timezone
 from django.contrib.auth.models import User
 from dateutil.relativedelta import relativedelta
 
+from sudokugame.views import create_board, create_daily_challenge
 
 def populate():
+    # Ensure results can be replicated
+    random.seed(10)
+
     # List of dictionaries containing data belonging to a model
 
-    # Random username and scores
-    users = [
-        {'username': 'TestUsername1',
-         'email': 'testusername1@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername2',
-         'email': 'testusername2@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername3',
-         'email': 'testusername3@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername4',
-         'email': 'testusername4@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername5',
-         'email': 'testusername5@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername6',
-         'email': 'testusername7@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername8',
-         'email': 'testusername1@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-         {'username': 'TestUsername9',
-         'email': 'testusername9@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername10',
-         'email': 'testusername10@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername11',
-         'email': 'testusername11@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername12',
-         'email': 'testusername12@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername13',
-         'email': 'testusername13@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername14',
-         'email': 'testusername14@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-        {'username': 'TestUsername15',
-         'email': 'testusername15@test.com',
-         'password': 'TtEeSsTtPpAaSsWwOoRrDd123'},
-    ]
-    scoreEasy = [1223, 1244, 4144, 5551, 1233, 54234, 554234, 12313, 41241, 2342, 1231, 4124, 74655, 100]
-    scoreMedium =[1223, 4124, 74655, 100, 41241, 554234, 1244,  2342, 1231, 1233, 54234, 4144, 5551, 12313,]
-    scoreHard = [4124, 74655, 100, 1223,  4144, 5551, 12313, 41241, 554234, 1244,  2342, 1231, 1233, 54234]
-    scoreDC = [1223,  554234, 4124,4144, 5551, 74655, 41241, 2342, 1231, 1233, 100,1244,  12313, 54234]
-
-    # different dates to test teh daily, weekly and monthly leaderboard
-    date = timezone.now()
-    dates = [date, date - relativedelta(days=1), date - relativedelta(weeks=1), date - relativedelta(months=1)]
-    difficultiesC = 'EMH'
-    boards = []
-
-    for x in difficultiesC:
-
-        board_base = generate(x, 10)
-
-        board = board_base.board
-        solution = board_base.solve().board
-
-        flattened_board = flatten_join(board)
-        flattened_solution = flatten_join(solution)
-
-        boardDict = {'grid': flattened_board,
-                         'solution': flattened_solution,
-                         'difficulty': x}
-
-
-        boards.append(boardDict)
-
-
-
-
+    # Create 14 usernames to test with
+    users = [{'username': f'testusername{i}', 'email': f'testusername{i}@test.com', 'password': 'testPassword'} for i in
+             range(14)]
 
     # Add the users into the model
+    users_list = []  # To store the created users to be used afterwards
     for userData in users:
-            u = User.objects.get_or_create(username=userData['username'], email=userData['email'])[0]
-            u.password = userData['password']
-            u.save()
-            print(u)
+        u, c = User.objects.get_or_create(username=userData['username'], email=userData['email'])
+        if c:
+            u.set_password(userData['password'])
+        u.save()
+        users_list.append(u)
+        print(f'Made user {u.username}')
 
-    # Add boards in to the model for easy, medium and hard difficulty
-    for boardData in boards:
+    # Generate 75 games for normal boards, each with a submission date 4 hours before the other,
+    # starting from the current time. This will allow us the check the time limit filter in the leaderboards.
+    print("Generating normal games")
+    time = timezone.now()
+    for i in range(75):
+        difficulty = random.choice("EMH") # Difficulty between Easy, Medium, Hard
+        user = random.choice(users_list)
+        board = create_board(difficulty)  # Use create_board from sudokugame app as it's simpler
+        if bool(board_query := Board.objects.filter(grid=board.grid)):
+            board = board_query[0]
+        else:
+            board.save()
 
-        b = addBoards(boardData['grid'], boardData['solution'], boardData['difficulty'], None)
+        print(f"Created board {board}")
+        simulate_game(board, user, time)
+        time -= relativedelta(hours=4)
 
-        print(b)
-        counter = 0
-        scoreCounter = 0
+    # Generate results for a daily challenge game
+    # For first 7 users, generate a result of zero, this should prevent them from partaking in the challenge for today
+    # but should also not show up in the leaderboard
+    challenge_board = create_daily_challenge()
+    print("Generating failed daily challenge attempts")
+    for i in range(0, 7):
+        addGame(challenge_board, user=users_list[i], score=0, date=timezone.now())
+        print(f"")
 
-        for user in users:
-            username = User.objects.get_by_natural_key(user.get('username'))
-
-            # The scores are added based on different difficulties.
-            if b.difficulty == 'M':
-                g = addGame(b, username, scoreMedium[scoreCounter], dates[counter])
-            elif b.difficulty == 'E':
-                g = addGame(b, username, scoreEasy[scoreCounter], dates[counter])
-            elif b.difficulty == 'H':
-                g = addGame(b, username, scoreHard[scoreCounter], dates[counter])
-
-            counter += 1
-            scoreCounter += 1
-            if counter > 3:
-                counter = 0
-            if scoreCounter > 13:
-                scoreCounter = 0
-            print(g)
-
-
-    # create new board for daily challenge
-    board_base = generate('M', 11)
-
-    board = board_base.board
-    solution = board_base.solve().board
-
-    flattened_board = flatten_join(board)
-    flattened_solution = flatten_join(solution)
-    b = addBoards(flattened_board, flattened_solution, None, date)
-    print(b)
-    for user in users:
-        username = User.objects.get_by_natural_key(user.get('username'))
-        g = addGame(b, username, scoreDC[scoreCounter], dates[counter])
-        scoreCounter +=1
-        counter+=1
-        if counter > 3:
-            counter = 0
-        if scoreCounter > 13:
-            scoreCounter = 0
-        print (g)
+    # For the remaining seven, let's just simulate a normal game
+    print("Generating successful daily challenge attempts")
+    for i in range(7, 14):
+        simulate_game(board=challenge_board, user=users_list[i], submission_date=timezone.now())
 
 
-
-def addBoards(grid, solution, difficulty, postedDate):
-    b = Board.objects.get_or_create(grid=grid, solution=solution, difficulty=difficulty,postedDate=postedDate )[0]
-    b.save()
-    return b
-
-
-
+    # Generate empty results, these should not be shown in the database
+def simulate_game(board, user, submission_date):
+    time_taken = random.randint(50, 20 * 60)  # Number of seconds user took for game
+    score = generate_score(time_taken, random.randint(0, 3), random.randint(1, 3))
+    game = addGame(board, user, score, submission_date)
+    print(game)
 
 def addGame(b, user, score, date):
-    g = Game.objects.get_or_create(board=b, user=user, score=score, submissionDate= date)[0]
+    g = Game(board=b, user=user, score=score, submissionDate=date)
+    # If a game with this board and user has already been created, then let us remake it
+    if bool(gameCheck := Game.objects.filter(board=b, user=user)):
+        g = gameCheck[0]
+    else:
+        g = Game(board=b, user=user)
+
+    g.score = score
+    g.submissionDate = date
     g.save()
+
     return g
 
 

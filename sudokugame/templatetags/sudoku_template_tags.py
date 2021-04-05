9068761f95d42d73e8d9e1ba4c6a093a03e56786
Danial Tariq <danial09.dt@gmail.com>
2021-04-05 13:29:32 +0100

    Make board generation more robust.

--- sudokugame/templatetags/sudoku_template_tags.py
+++ sudokugame/templatetags/sudoku_template_tags.py
@@ -7,9 +7,8 @@ register = template.Library()
 
 
 @register.inclusion_tag('sudokugame/board.html')
-def get_board(board=None, additional_cell_classes=''):
-    if board is None:
-        x = [['0' for i in range(9)] for i in range(9)]
-        return {"grid": [['0' for i in range(9)] for i in range(9)], "cell_classes": additional_cell_classes}
-    else:
-        return {"grid": unflatten_split(board.grid, str, '0'), "cell_classes": additional_cell_classes}
+def get_board(board=None, additional_cell_classes='', additional_board_classes=''):
+    flattened = unflatten_split(board.grid, str, '0') if board else [['0' for i in range(9)] for i in range(9)]
+    return {"grid": flattened,
+            "cell_classes": additional_cell_classes,
+            "board_classes": additional_board_classes}
